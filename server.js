c62c7bf485717ef3449dc800d401592356be779b
Cynthia Yeung <cyeung91@gmail.com>
2015-04-07 20:23:16 -0700

    set up twitter authentication and socket.io

--- server.js
+++ server.js
@@ -18,9 +18,55 @@ app.use(methodOverride('_method'));
 // to include css/js/imgs
 app.use(express.static(__dirname + '/public'));
 
+// twitter api for userTimeline
+var T = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+});
+
 // get root and render index
 app.get('/', function(req, res){
   res.render('index.ejs');
+});
+
+// io connection for real-time submit
+io.on('connection', function(socket){
+  console.log('user connected');
+
+  T.get('users/show', { screen_name: 'wdi16_goodvibes' }, function(err, data, response) {
+    // console.log(data);
+
+    createDate = data.status.created_at;
+    tweet = data.status.text;
+
+    createDate.split(' ').splice(0,3).join(' ');
+    var split = createDate.split(' ');
+    var date = split.splice(0,3);
+    var year = split[split.length-1];
+    date.push(year);
+    date = date.join(' ');
+
+    socket.emit('date', date);
+    socket.emit('manifest', tweet);
+  });
+
+  var stream = T.stream('statuses/filter', { track: ['good vibes', 'positive vibes', 'I\'m grateful'], language: 'en', filter_level: 'low' });
+    stream.on('tweet', function (tweet) {
+    console.log(tweet.text);
+    socket.emit('stream', tweet.text);
+  });
+
+
+  socket.on('manifest response', function(msg){
+    io.emit('manifest response', msg);
+  });
+
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+    stream.stop();
+  });
 });
 
 http.listen(3000, function(){
